3de42b32f3490b36ea7512e9fece87bc6a7b263d
Timothy Threet <timothythreet@Timothys-MacBook-Pro.local>
2022-10-13 13:22:34 -0400

    completed invoices routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -17,6 +17,8 @@ router.get('/', async function(req, res, next) {
 	}
 });
 
+// Get single invoice with :id
+
 router.get('/:id', async function(req, res, next) {
 	try {
 		let id = req.params.id;
@@ -31,12 +33,82 @@ router.get('/:id', async function(req, res, next) {
 	}
 });
 
-// Get single invoice with :id
-
 // Add an invoice
+router.post('/', async function(req, res, next) {
+	try {
+		let { comp_code, amt } = req.body;
+		const result = await db.query(
+			`INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+			[ comp_code, amt ]
+		);
+		return res.status(201).json({ invoice: result.rows[0] });
+	} catch (e) {
+		return next(e);
+	}
+});
 
 // update an invoice
+router.put('/:id', async function(req, res, next) {
+	try {
+		let { amt, paid } = req.body;
+		let id = req.params.id;
+		let paidDate = null;
 
+		const currResult = await db.query(
+			`SELECT paid
+             FROM invoices
+             WHERE id = $1`,
+			[ id ]
+		);
+
+		if (currResult.rows.length === 0) {
+			throw new ExpressError(`No such invoice: ${id}`, 404);
+		}
+
+		const currPaidDate = currResult.rows[0].paid_date;
+
+		if (!currPaidDate && paid) {
+			paidDate = new Date();
+		}
+		else if (!paid) {
+			paidDate = null;
+		}
+		else {
+			paidDate = currPaidDate;
+		}
+
+		const result = await db.query(
+			`UPDATE invoices
+             SET amt=$1, paid=$2, paid_date=$3
+             WHERE id=$4
+             RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+			[ amt, paid, paidDate, id ]
+		);
+
+		return res.json({ invoice: result.rows[0] });
+	} catch (err) {
+		return next(err);
+	}
+});
 // delete an invoice
+router.delete('/:id', async function(req, res, next) {
+	try {
+		let id = req.params.id;
 
+		const result = await db.query(
+			`DELETE FROM invoices
+             WHERE id = $1
+             RETURNING id`,
+			[ id ]
+		);
+
+		if (result.rows.length === 0) {
+			throw new ExpressError(`No such invoice: ${id}`, 404);
+		}
+
+		return res.json({ status: 'deleted' });
+	} catch (err) {
+		return next(err);
+	}
+});
 module.exports = router;
